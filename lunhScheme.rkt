099049f213446b94b3c9af0918567d72c1681e9f
darienmiller88 <32966645+darienmiller88@users.noreply.github.com>
2019-08-12 19:20:46 -0400

    Update lunhScheme.rkt

--- lunhScheme.rkt
+++ lunhScheme.rkt
@@ -1,3 +1,4 @@
+#lang racket
 (define luhnAlgorithm	
 	
   ;Start of lambda function
@@ -6,8 +7,8 @@
    ;loop over each number in the credit card number the user passed in
     (let loop ((creditCardNumber n) (i 0)(result 0)) 
       (if (= 0 creditCardNumber)
-          (= 0 (remainder r 10)) 
+          (= 0 (remainder result 10)) 
           (loop (quotient creditCardNumber 10) (+ i 1)
-                (+ result (if (even? i)A (remainder num 10)
-                       (let ((part (* 2 (remainder num 10))))
+                (+ result (if (even? i) (remainder creditCardNumber 10)
+                       (let ((part (* 2 (remainder creditCardNumber 10))))
                          (+ (remainder part 10) (quotient part 10))))))))))
